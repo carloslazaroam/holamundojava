3351f25e725834e29cd875d51a922b363d5bc2e1
a073604881m <a073604881m@S09-PC08>
2021-11-22 19:22:58 +0100

    este es un ejemplo de prueba hola mundo

---
+++ holamundojava/src/holamundojava/Holamundojava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundojava;
+
+/**
+ *
+ * @author Carlos Lázaro <carloslazaroam@gmail.com>
+ */
+public class Holamundojava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
